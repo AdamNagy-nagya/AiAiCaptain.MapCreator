e16f0537ba70e7874ee21ddbc5d4e7a2efb14dfa
anagy <a.nagy@mito.hu>
2019-04-26 14:59:13 +0200

    Init project

---
+++ Assets/MapSaveScript.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapSaveScript : MonoBehaviour
+{
+    public Tilemap Tilemap;
+    public String Mapname;
+    public int X;
+    public int Y;
+    void Start()
+    {
+        string path = "Assets/SavedMaps/" + Mapname + ".txt";
+
+        Tile[] tiles = TilemapExtensions.GetTiles<Tile>(Tilemap);
+        String serialMapString = "";
+        int cnt = tiles.Length;
+        foreach (Tile tile in tiles)
+        {
+            serialMapString = serialMapString + tile.sprite.name;
+            if (--cnt != 0)
+            {
+                serialMapString = serialMapString + ";";
+            }
+            Debug.Log(serialMapString);
+        }
+
+        File.WriteAllText(path, string.Empty);
+
+
+        StreamWriter writer = new StreamWriter(path, true);
+        writer.Write(X + ";" + Y + ";");
+        writer.Write(serialMapString);
+        writer.Close();
+
+        AssetDatabase.ImportAsset(path);
+    }
+}
